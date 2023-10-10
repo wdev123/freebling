cf7cba5ea94db4c9fc13ff24a5e7d3ce04a0460e
devwebok123 <dev.webok123@gmail.com>
2023-10-09 19:04:31 -0500

    free

---
+++ configuration.ts
@@ -0,0 +1,14 @@
+const configuration = {
+  firebase: {
+    apiKey: "AIzaSyAZ2_jhLdlDgLt7TmppxevnADafiP9wVXY",
+    authDomain: "test-firebase-key-2a0b2.firebaseapp.com",
+    projectId: "test-firebase-key-2a0b2",
+    storageBucket: "test-firebase-key-2a0b2.appspot.com",
+    appId: "1:372160452180:web:321ce4ab4f3377dfe9bfc1",
+    messagingSenderId: "G-SC35JN445S"
+  },
+  emulatorHost: "",
+  emulator: false
+};
+
+export default configuration;
---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
+}
+
+module.exports = nextConfig
